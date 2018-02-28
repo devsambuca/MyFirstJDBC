7a7d4a5071122e6ca528496d11caf0c2074eea3d
Fominykh <stoyakovi4@gmail.com>
2018-03-01 00:39:08 +0300

    Added class ApplicationJDBC

---
+++ src/main/java/ApplicationJDBC.java
@@ -0,0 +1,56 @@
+/**
+ * Created by smb on 01.03.2018.
+ */
+import java.sql.*;
+public class ApplicationJDBC {
+    /**
+     * JDBC Driver and database url
+     */
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DATABASE_URL = "jdbc:mysql://localhost/mytestdb";
+
+    /**
+     * User and Password
+     */
+    static final String USER = "root";
+    static final String PASSWORD = "root";
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Connection connection = null;
+        Statement statement = null;
+
+        System.out.println("Registering JDBC driver...");
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        System.out.println("Creating database connection...");
+        connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+
+        System.out.println("Executing statement...");
+        statement = connection.createStatement();
+
+        String sql;
+        sql = "SELECT * FROM developers";
+
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        System.out.println("Retrieving data from database...");
+        System.out.println("\nDevelopers:");
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            int salary = resultSet.getInt("salary");
+
+            System.out.println("\n================\n");
+            System.out.println("id: " + id);
+            System.out.println("Name: " + name);
+            System.out.println("Salary: $" + salary);
+        }
+
+        System.out.println("Closing connection and releasing resources...");
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+}
+
