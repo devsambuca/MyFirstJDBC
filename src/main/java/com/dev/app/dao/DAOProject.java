d937daf7f08bb3d8023d2e92f17254febe70043a
Fominykh <stoyakovi4@gmail.com>
2018-03-11 15:44:08 +0300

    Added methods create and read

--- src/main/java/com/dev/app/dao/DAOProject.java
+++ src/main/java/com/dev/app/dao/DAOProject.java
@@ -5,6 +5,7 @@ import com.dev.app.model.Project;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -17,6 +18,7 @@ public class DAOProject implements GenericDAO<Project> {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setLong(1, project.getId());
             preparedStatement.setString(2, project.getName());
+            preparedStatement.setInt(3, project.getCost());
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -24,6 +26,20 @@ public class DAOProject implements GenericDAO<Project> {
 
     @Override
     public Project read(long id) {
+        String sql = "SELECT * FROM project WHERE id = ?";
+        try (Connection connection = ApplicationJDBC.getConnection()){
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                int cost = resultSet.getInt("cost");
+                System.out.println("id № " + id + name + " " + cost);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
--- src/main/java/com/dev/app/model/Project.java
+++ src/main/java/com/dev/app/model/Project.java
@@ -3,6 +3,7 @@ package com.dev.app.model;
 public class Project {
     private long id;
     private String name;
+    private int cost;
 
     public long getId() {
         return id;
@@ -18,5 +19,9 @@ public class Project {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getCost() {
+        return cost;
     }
 }
